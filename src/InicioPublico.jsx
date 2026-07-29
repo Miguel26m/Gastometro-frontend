147fde65c9f316917fa2fd3b746b7cf783497088
meixueirocruzarturodaniel <meixueirocruzarturodaniel@gmail.com>
2026-07-29 09:31:07 -0600

    revision

--- src/InicioPublico.jsx
+++ src/InicioPublico.jsx
@@ -53,9 +53,7 @@ function InicioPublico() {
                 </h2>
                 
                 <div className="bg-white p-6 rounded-2xl shadow-md border border-gray-100 max-w-3xl text-center mb-10">
-                    <p className="text-lg text-gray-700 font-medium mb-4">
-                        Gastómetro es la herramienta definitiva para gestionar todos tus gastos recurrentes en plataformas de streaming, software y servicios.
-                    </p>
+                  
                     <p className="text-lg text-gray-700 font-medium">
                         Descubre ofertas exclusivas, unifica tus fechas de pago y evita cobros sorpresa mes a mes.
                     </p>
